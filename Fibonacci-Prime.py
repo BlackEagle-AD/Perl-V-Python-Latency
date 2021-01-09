ef870b8852fd9d12cd6fb9b08280d2ebc63bdea3
BEAD <59107505+BlackEagle-AD@users.noreply.github.com>
2021-01-09 13:12:25 -0500

    Update Fibonacci-Prime.py

--- Fibonacci-Prime.py
+++ Fibonacci-Prime.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
+# Black Eagle Analytics & Data LLC
+# Written by Shane Bruce 28th of Aug 2019
+# as a speed and capability comparison
+# of Perl v Python :  test.perl and test.python
+
 import time
-print("Hello")
 x=int(input("Limit to Fibonacci:"))
 count1 = 0
 count2 = 0
@@ -8,25 +12,19 @@ a, b = 0, 1
 
 start = time.time()
 while a < x:
-	count2 +=1
-    	print a,
-    	prime = True
-    	for n in range (2,a):
-        		if a % n == 0:
-            		print ': equals', n, '*', a//n,
-            			prime = False
-            			Break
-    	if prime and a > 1:
-      		 print ': Prime number',
-       		count1+=1
-    	a, b = b, a+b
-    	Print
+    count2 = count2 + 1
+    prime = True
+    for n in range(2,a):
+        if a % n == 0:
+            print(a,': equals', n, '*', a//n,)
+            prime = False
+            break
+    if prime and a > 1:
+        print(a,': Prime number',)
+        count1+=1
+    a, b = b, a+b
 
-Print
 etime = (time.time()-start)*1000
-print "Elasped time:%8.3f" % etime,
-print "MillSec"
-print 'For', x, 'there are', count2, "values"
-print 'in the Fibonacci Sequence,'
-print 'of which', count1, 'are Prime Numbers.'
-Print
+print ("Elasped time:%8.3f" % etime,"MillSec")
+print ('For', x, 'there are', count2, "values in the Fibonacci Sequence, of which")
+print (count1, 'are Prime Numbers.')
